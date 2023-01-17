eebcd32578ac43116a7ebc59a9f62b566e065307
Verdant31 <joao.pedro3110@hotmail.com>
2023-01-16 22:46:06 -0400

    creating header; creating sidebar

---
+++ src/components/Home/Header/components/DrawerItem.tsx
@@ -0,0 +1,39 @@
+import React, { FC } from "react";
+import { Accordion, AccordionDetails, AccordionSummary } from "@mui/material";
+import Link from "next/link";
+import { CaretDown } from "phosphor-react";
+
+interface DrawerItemProps {
+  title: string;
+  items: string[];
+}
+
+const DrawerItem: FC<DrawerItemProps> = ({ items, title }) => {
+  return (
+    <Accordion
+      className="border-none shadow-none before:content-none"
+      elevation={0}
+      disableGutters
+    >
+      <AccordionSummary
+        expandIcon={<CaretDown color="black" />}
+        className="m-0 h-8 min-h-0 p-0"
+        aria-controls="panel1d-content"
+        id="panel1d-header"
+      >
+        <Link href="#" className="text-lg font-semibold text-zinc-600">
+          {title}
+        </Link>
+      </AccordionSummary>
+      <AccordionDetails className=" flex flex-col gap-2 p-0">
+        {items.map((item) => (
+          <Link key={item} href="#">
+            {item}
+          </Link>
+        ))}
+      </AccordionDetails>
+    </Accordion>
+  );
+};
+
+export default DrawerItem;
